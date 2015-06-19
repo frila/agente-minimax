34f6bbcf67f44a1030a6ac00e2604fdf2bb95b17
Fausto Ferreira Junqueira <faustoferjunqueira@gmail.com>
2015-06-19 12:57:02 -0300

    Implement Minimax.minimax

--- minimax.py
+++ minimax.py
@@ -11,8 +11,7 @@ class Heuristic:
 
 class Minimax:
   def __init__(self, me, challenger):
-    self.me = me
-    self.challenger = challenger
+    self.me, self.challenger = me, challenger
 
   def heuristic(self, board, color):
     if color == self.color_me:
@@ -26,8 +25,27 @@ class Minimax:
     else:
       return self.challenger.eval(vector_values)
 
-  def change_color(self):
+  def change_color(self, color):
     if color == self.me.color:
       return self.challenger.color
     else:
       return self.me.color
+
+  def children(self, board, color):
+    return board.valid_moves(color)
+
+  def clone_board(self, board):
+    return board.get_clone()
+
+  def minimax(self, board, depth, color):
+    if depth == 0:
+      return self.heuristic(board,color)
+    else:
+      children = self.children(board, color)
+      results = []
+      for child in children:
+        board_clone = self.clone_board(board)
+        self.board.play(child, color)
+        results[] = self.minimax(board_clone, depth-1, self.change_color(color))
+      return self.calculate_min_or_max(vector_values, color)
+
