77cf731da51994fd12e907b247c8615be5caec7d
Fausto Ferreira Junqueira <faustoferjunqueira@gmail.com>
2015-06-19 14:06:35 -0300

    Apply heuristic for new minimax

--- models/players/heuristic_test.py
+++ models/players/heuristic_test.py
@@ -3,28 +3,39 @@ from models.algorithm.minimax import Minimax
 from models.board import Board
 import random
 
-class HeuristicTestBlack(Heuristic):
+class HeuristicTestMe(Heuristic):
 
-  def __init__(self):
-    Heuristic.__init__(Board.BLACK)
-
-  def heuristic(self, board, color):
-    return random.randrange(-20, 20)
-
-  def eval(self, vector):
-    #Lembre-se, uma heuristica tem que ser min, e o outro deve ser max
-    #return max(vector)
-    return min(vector)
-
-class HeuristicTestWhite(Heuristic):
-
-  def __init__(self):
-    Heuristic.__init__(Board.WHITE)
+  def __init__(self, color):
+    Heuristic.__init__(color)
 
   def heuristic(self, board, color):
     return random.randrange(-20, 20)
 
-  def eval(self, vector):
+  def eval(self, old_value, new_value):
     #Lembre-se, uma heuristica tem que ser min, e o outro deve ser max
-    return max(vector)
-    #return min(vector)
+    return old_value > new_value # isso representa o max 
+
+class HeuristicTestChallenger(Heuristic):
+
+  def __init__(self, color):
+    Heuristic.__init__(color)
+
+  def heuristic(self, board, color):
+    return random.randrange(-20, 20)
+
+  def eval(self, old_value, new_value):
+    #Lembre-se, uma heuristica tem que ser min, e o outro deve ser max
+    return old_value < new_value # isso representa o min 
+    
+
+class MinimaxPlayer:
+  def __init__(self, color):
+    self.color = color
+    self.heuristic_me = HeuristicTestMe(color)
+    if color == Board.BLACK:
+      self.heuristic_challenger = HeuristicTestChallenger(Board.WHITE)
+    else:
+      self.heuristic_challenger = HeuristicTestChallenger(Board.BLACK)
+    
+  def play(self, board):
+    return 
