c3f8406869aea77ec9ac4616aa26153e161b6c54
Fausto Ferreira Junqueira <faustoferjunqueira@gmail.com>
2015-06-19 15:19:19 -0300

    Change name of player heuristic

--- models/players/heuristic_player.py
+++ models/players/heuristic_player.py
@@ -1,6 +1,6 @@
 from models.heuristics.heuristic_test import *
 from models.algorithm.minimax import MinimaxPlayer
 
-class TestPlayer(MinimaxPlayer):
+class HeuristicPlayer(MinimaxPlayer):
   def __init__(self, color):
     MinimaxPlayer.__init__(color, 5, HeuristicTestMe(), HeuristicTestChallenger())
